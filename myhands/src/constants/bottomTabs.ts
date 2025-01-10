3ee4aef683690d60dfb56e01e11aac2fc75d6f0b
Iseo Choi <chtoqur@gmail.com>
2025-01-10 15:30:30 +0900

    [design] 네비게이션 기본 스타일 수정(#4)

---
+++ myhands/src/constants/bottomTabs.ts
@@ -0,0 +1,32 @@
+import {icons} from '@/assets/icons/bottomTabsIcons';
+import BoardDetailScreen from '@/screens/board/BoardDetailScreen';
+import MypageHomeScreen from '@/screens/mypage/MypageHomeScreen';
+import QuestHomeScreen from '@/screens/quest/QuestHomeScreen';
+import RaceHomeScren from '@/screens/race/RaceHomeScren';
+
+export const bottomTabs = [
+  {
+    name: 'Mypage',
+    comp: MypageHomeScreen,
+    inactiveIcon: icons.my,
+    activeIcon: icons.my_focus,
+  },
+  {
+    name: 'Quest',
+    comp: QuestHomeScreen,
+    inactiveIcon: icons.quest,
+    activeIcon: icons.quest_focus,
+  },
+  {
+    name: 'Race',
+    comp: RaceHomeScren,
+    inactiveIcon: icons.race,
+    activeIcon: icons.race_focus,
+  },
+  {
+    name: 'Board',
+    comp: BoardDetailScreen,
+    inactiveIcon: icons.board,
+    activeIcon: icons.board_focus,
+  },
+];
