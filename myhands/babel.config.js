4c10c4ac00ca2fcb2b3f9ae1d36f09ecb6aa5ce2
Iseo Choi <chtoqur@gmail.com>
2025-01-10 15:28:02 +0900

    [style] 린트 및 프리티어 설정 변경

--- myhands/babel.config.js
+++ myhands/babel.config.js
@@ -5,6 +5,8 @@ module.exports = {
     [
       'module-resolver',
       {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
         alias: {
           '@': './src',
         },
