a8d886221ad2a184d5db1897d4cd65ff49e9bd92
Iseo Choi <chtoqur@gmail.com>
2025-01-12 15:15:13 +0900

    [feat] 리액트 쿼리 삭제제

--- myhands/App.tsx
+++ myhands/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import {DefaultTheme, NavigationContainer} from '@react-navigation/native';
-import {QueryClientProvider} from '@tanstack/react-query';
-import queryClient from '@/api/queryClient';
 import RootNavigator from '@/navigations/root/RootNavigator';
 
 const AppTheme = {
@@ -14,11 +12,9 @@ const AppTheme = {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <NavigationContainer theme={AppTheme}>
-        <RootNavigator />
-      </NavigationContainer>
-    </QueryClientProvider>
+    <NavigationContainer theme={AppTheme}>
+      <RootNavigator />
+    </NavigationContainer>
   );
 }
 
