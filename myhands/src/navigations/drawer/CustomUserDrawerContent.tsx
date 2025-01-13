8905399123046ed6985ca2200c08949d5cb0c6c5
김현지 <chik1y@naver.com>
2025-01-14 01:45:03 +0900

    [feat] drawer 일반 유저 UI 구현(#19)

--- myhands/src/navigations/drawer/CustomUserDrawerContent.tsx
+++ myhands/src/navigations/drawer/CustomUserDrawerContent.tsx
@@ -1,42 +1,111 @@
-import {StyleSheet, Text} from 'react-native';
+import React from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 import {
   DrawerContentComponentProps,
   DrawerContentScrollView,
   DrawerItemList,
 } from '@react-navigation/drawer';
-import * as St from './CustomUserDrawerContent.style';
+import {colors} from '@/constants';
+import {characterImages} from '@/constants/character';
 import {useAuthStore} from '@/store/authStore';
 
 function CustomUserDrawerContent(props: DrawerContentComponentProps) {
-  const {user} = useAuthStore();
+  const user = useAuthStore(state => state.user);
 
   return (
-    <St.Container>
-      <DrawerContentScrollView
-        {...props}
-        scrollEnabled={false}
-        contentContainerStyle={styles.container}
+    <View style={{flex: 1}}>
+      <LinearGradient
+        colors={['#FFFFFF', '#FFE4DD']}
+        style={styles.gradientBackground}
       >
-        <St.UserInfoContainer>
-          <Text>사용자 정보란</Text>
-          <St.NameText>{user?.name}</St.NameText>
-          <St.NameText>{user?.id}</St.NameText>
-          <St.NameText>{user?.employeeNum}</St.NameText>
-          <St.NameText>{user?.department}</St.NameText>
-          <St.NameText>{user?.joinedAt}</St.NameText>
-          <St.NameText>{user?.level}</St.NameText>
-        </St.UserInfoContainer>
+        <View style={styles.container}>
+          <View style={styles.avatarContainer}>
+            <Image
+              source={characterImages[user?.avartaId || 1]}
+              style={styles.avatar}
+            />
+          </View>
+
+          <View style={styles.nameContainer}>
+            <Text style={styles.name}>{user?.name || '사용자 이름'}</Text>
+            <Text style={styles.boldText}>{user?.id || 'username'}</Text>
+          </View>
+
+          <View style={styles.infoContainer}>
+            <Text style={styles.infoText}>
+              사{'   '}번{'  '}
+              <Text style={styles.boldText}>
+                {user?.employeeNum || '000000'}
+              </Text>
+            </Text>
+            <Text style={styles.infoText}>
+              소{'   '}속{'  '}
+              <Text style={styles.boldText}>
+                {user?.department || '부서명'}
+              </Text>
+            </Text>
+            <Text style={styles.infoText}>
+              입사일{'  '}
+              <Text style={styles.boldText}>
+                {user?.joinedAt || '0000.00.00'}
+              </Text>
+            </Text>
+            <Text style={styles.infoText}>
+              레{'   '}벨{'   '}
+              <Text style={styles.boldText}>{user?.level || 'F1-II'}</Text>
+            </Text>
+          </View>
+        </View>
+      </LinearGradient>
+      <DrawerContentScrollView {...props}>
         <DrawerItemList {...props} />
       </DrawerContentScrollView>
-    </St.Container>
+    </View>
   );
 }
 
+export default CustomUserDrawerContent;
+
 const styles = StyleSheet.create({
+  gradientBackground: {
+    flex: 0.65,
+  },
   container: {
-    flex: 1,
-    margin: 0,
+    flex: 0.4,
+    paddingVertical: 36,
+    paddingHorizontal: 36,
+    alignItems: 'flex-start',
+  },
+  avatarContainer: {
+    marginBottom: 12,
+  },
+  avatar: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    borderWidth: 2,
+    borderColor: colors.BLACK,
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: colors.BLACK,
+    marginBottom: 4,
+  },
+  infoContainer: {
+    width: '100%',
+  },
+  infoText: {
+    fontSize: 14,
+    color: colors.GRAY_700,
+    marginBottom: 6,
+  },
+  boldText: {
+    fontWeight: 'bold',
+    color: colors.GRAY_700,
+  },
+  nameContainer: {
+    marginBottom: 16,
   },
 });
-
-export default CustomUserDrawerContent;
