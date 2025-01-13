c83be050fa0b70a6b43f84cf3bc93db97c0f3df1
김현지 <chik1y@naver.com>
2025-01-14 02:53:01 +0900

    [feat] drawer 어드민 구현(#19)

--- myhands/src/navigations/drawer/AdminDrawerNavigator.tsx
+++ myhands/src/navigations/drawer/AdminDrawerNavigator.tsx
@@ -1,18 +1,49 @@
-import React from 'react';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import React, {useState} from 'react';
+import {Dimensions} from 'react-native';
+import {
+  createDrawerNavigator,
+  DrawerNavigationProp,
+} from '@react-navigation/drawer';
+import {RouteProp} from '@react-navigation/native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import BottomTabsNavigator from '../bottomTabs/BottomTabsNavigator';
+import CustomModal from '@/components/_modal/CustomModal';
 import {adminNavigations} from '@/constants';
+import CustomAdminDrawerContent from '@/navigations/drawer/CustomAdminDrawerContent';
 import AdminHomeScreen from '@/screens/admin/AdminHomeScreen';
-import ChangeProfileScreen from '@/screens/settings/ChangeProfileScreen';
+import ChangePasswordScreen from '@/screens/settings/ChangePasswordScreen';
 import {useAuthStore} from '@/store/authStore';
 
 export type AdminDrawerParamList = {
   [adminNavigations.ADMIN_HOME]: undefined;
+  Logout: undefined;
 };
 
 const Drawer = createDrawerNavigator();
 
-function AdminDrawerNavigator() {
+interface AdminDrawerNavigatorProps {
+  navigation: DrawerNavigationProp<AdminDrawerParamList>;
+}
+
+function DrawerIcons({
+  route,
+  size,
+}: {
+  route: RouteProp<AdminDrawerParamList, keyof AdminDrawerParamList>;
+  size: number;
+}) {
+  switch (route.name) {
+    case adminNavigations.ADMIN_HOME:
+      return <AntDesign name="home" size={size} />;
+    default:
+      return <MaterialIcons name="logout" size={size} />;
+  }
+}
+
+function AdminDrawerNavigator({navigation}: AdminDrawerNavigatorProps) {
   const logout = useAuthStore(state => state.logout);
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
 
   const handleLogout = async () => {
     try {
@@ -23,31 +54,64 @@ function AdminDrawerNavigator() {
   };
 
   return (
-    <Drawer.Navigator
-      initialRouteName={adminNavigations.ADMIN_HOME}
-      screenOptions={{
-        drawerType: 'front',
-        headerShown: true,
-        headerTitleAlign: 'center',
-      }}
-    >
-      <Drawer.Screen
-        name={adminNavigations.ADMIN_HOME}
-        component={AdminHomeScreen}
-        options={{drawerLabel: '메인 화면으로'}}
-      />
-      <Drawer.Screen
-        name="logout"
-        component={ChangeProfileScreen}
-        options={{drawerLabel: '로그아웃'}}
-        listeners={{
-          drawerItemPress: e => {
-            e.preventDefault();
-            handleLogout();
+    <>
+      <Drawer.Navigator
+        drawerContent={CustomAdminDrawerContent}
+        initialRouteName="BottomTabs"
+        screenOptions={({route}) => ({
+          drawerType: 'front',
+          drawerStyle: {
+            width: Dimensions.get('screen').width * 0.7,
           },
-        }}
+          headerShown: false,
+          drawerIcon: ({size}) =>
+            DrawerIcons({
+              route: route as RouteProp<
+                AdminDrawerParamList,
+                keyof AdminDrawerParamList
+              >,
+              size,
+            }),
+        })}
+      >
+        <Drawer.Screen
+          name="BottomTabs"
+          component={BottomTabsNavigator}
+          options={{
+            drawerItemStyle: {display: 'none'},
+          }}
+        />
+        <Drawer.Screen
+          name={adminNavigations.ADMIN_HOME}
+          component={AdminHomeScreen}
+          options={{drawerLabel: '메인 화면으로'}}
+          listeners={{
+            drawerItemPress: e => {
+              e.preventDefault();
+              navigation.navigate(adminNavigations.ADMIN_HOME);
+            },
+          }}
+        />
+        <Drawer.Screen
+          name="Logout"
+          component={ChangePasswordScreen}
+          options={{drawerLabel: '로그아웃'}}
+          listeners={{
+            drawerItemPress: e => {
+              e.preventDefault();
+              setModalOpen(true);
+            },
+          }}
+        />
+      </Drawer.Navigator>
+      <CustomModal
+        state="Logout"
+        type="logout"
+        isOpen={modalOpen}
+        onClose={() => setModalOpen(false)}
+        onButtonClick={handleLogout}
       />
-    </Drawer.Navigator>
+    </>
   );
 }
 
